9c2752acf7b051647d306fa5fa8a00a2e6eb38f4
Mxk01 <secretcoderz@yahoo.com>
2021-08-26 09:23:15 +0300

    =

--- src/components/Girl1.js
+++ src/components/Girl1.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Girl1.css'
 import {motion} from 'framer-motion'
 import {Link} from 'react-router-dom'
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 function Girl1() {
+  // when turn is true it's my turn ,otherwise anime girl's turn
+  let [turn,setTurn] = useState(false);
+  let [url,setURL] = useState("https://i.imgur.com/tfCChIH.png")
+  let [name,setName] = useState("")
+
     return (
         <div className="girl1__container">
             <Link to="/" style={{textDecoration:"none",color:'white'}}>Home</Link>
@@ -10,11 +16,32 @@ function Girl1() {
              initial={{opacity:0}}
              animate={{opacity:1}}    
              transition={{type:"tween" }}>
-
-               <h1>Siesta</h1>
+               
+               <h1>{name}</h1>
+               
                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, dolores! Eius harum esse quas eveniet. Temporibus id fugiat reiciendis voluptate error blanditiis rem porro quae atque voluptatum facere, voluptas animi.</span>
+               <DoubleArrowIcon className="girl1__forward"  onClick={()=>{
+                 
+                 setTurn(!turn)
+                 switch(turn)
+                 {
+                   case false:
+                     setURL('https://i.imgur.com/tfCChIH.png')
+                     setName('Siesta')
+                     break;
+
+                  case true: 
+                     setURL('https://i.imgur.com/PUOgST6.png')
+                     setName('Player')
+                     break;
+
+                  default:
+                    setURL('')
+                 }
+                 }}/> 
              </motion.div>
-              <motion.img className="girl1__render" src="https://i.imgur.com/tfCChIH.png" alt=""
+            
+              <motion.img className="girl1__render" src={url} alt=""
                 animate={{opacity:1}} initial ={{opacity:0}} transition={{delay:.2,type:'tween'}}
               />
         </div>
