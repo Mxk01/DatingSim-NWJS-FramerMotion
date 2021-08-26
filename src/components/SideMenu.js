59e48f3f3fb77ecabc61d0a460a95858a63e3672
Mxk01 <secretcoderz@yahoo.com>
2021-08-26 09:37:56 +0300

    =

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -20,10 +20,10 @@ function SideMenu() {
         className="sidemenu__container">
              <motion.span whileHover={{x:-10}} className="sidemenu__item">
                  <Link style={{textDecoration:"none",color:"white"}} to="/roadmap">Start</Link></motion.span>
-             <motion.span whileHover={{x:-10}} className="sidemenu__item">Load</motion.span>
-             <motion.span whileHover={{x:-10}} className="sidemenu__item">Preferences</motion.span>
-             <motion.span whileHover={{x:-10}} className="sidemenu__item">Help</motion.span>
-             <motion.span whileHover={{x:-10}} onClick={()=>window.close()}
+             <motion.span whileHover={{x:-15}} className="sidemenu__item">Load</motion.span>
+             <motion.span whileHover={{x:-20}} className="sidemenu__item">Preferences</motion.span>
+             <motion.span whileHover={{x:-25}} className="sidemenu__item">Help</motion.span>
+             <motion.span whileHover={{x:-30}} onClick={()=>window.close()}
              className="sidemenu__item">Quit</motion.span>
         </motion.div>
     )
